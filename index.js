77283635993adf8ec0b9f2860fd958303764aefc
dickyprtma <dickypratama.tech@gmail.com>
2025-05-03 11:03:59 +0700

    add bin/www and cors

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config()
+}
 
+const cors = require('cors');
 const express = require('express');
+
 const { router } = require('./routers');
 const errorHandler = require('./middleware/errorHandler');
 const app = express();
-const port = 3000
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/', router)
 
-
-
-
 app.use(errorHandler)
-app.listen(port, ()=> {
-    console.log(`Server is running on http://localhost:${port}`)
-})
+
+module.exports = app
