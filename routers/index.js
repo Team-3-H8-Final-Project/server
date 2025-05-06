bb0aac940e7f86d343d7ee8f894f10571187cdaa
dickyprtma <dickypratama.tech@gmail.com>
2025-05-06 10:06:39 +0700

    chore:testing generate grammar question & answer

--- routers/index.js
+++ routers/index.js
@@ -6,24 +6,28 @@ const authentication = require("../middleware/authentication");
 const FeedbackController = require("../controllers/FeedbackController");
 
 router.get("/", (req, res) => {
-  res.send("Welcome to ");
+    res.send("Welcome to ");
 });
 
 router.post("/login", UserController.login);
 router.post("/register", UserController.register);
 
+// grammar
 router.post("/generate", authentication, Controller.generateGrammar);
 router.get("/grammar", authentication, Controller.getQuestionsByLevel);
+
+// challenge
 router.post("/challenge", authentication, Controller.generateChallenge);
 router.get("/challenge", authentication, Controller.getChallenges);
+
 router.get("/profile", authentication, UserController.getProfile);
 router.post(
-  "/feedback/:type",
-  authentication,
-  FeedbackController.generateFeedback
+    "/feedback/:type",
+    authentication,
+    FeedbackController.generateFeedback
 ); //type : conversation | challenge | grammar
 router.get("/feedback", authentication, FeedbackController.getFeedback);
 router.get("/feedback/:id", authentication, FeedbackController.getFeedbackById);
 module.exports = {
-  router,
+    router,
 };
